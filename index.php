04c4bae961106027e1c053bfb6554fb21c556c88
jmoostland <jeanineoostland@gmail.com>
2017-11-17 22:21:43 +0100

    verbinding met het registratieformulier

--- index.php
+++ index.php
@@ -2,26 +2,25 @@
 
 <?php
 //session_start();
-
 //require 'verbindingMysql.php';
 
 
-if (isset($_POST['hidden'])){
+if (isset($_POST['hidden'])) {
     $inloggen = new mysqli('localhost', 'root', '', 'login');
-    $username=$_POST['userName'];
-    $password=$_POST['pSd'];
-    $sql= "SELECT * FROM `loginsystem` WHERE `uname`='".$username."'AND `psd`='".$password."'";
-    $result=$inloggen->query($sql);
-    $num = mysqli_num_rows ($result);
-    
-    if($num == 1){
-        $extra='welcome.php';
-        header("Location: $extra");
-    }else{
-    echo "wrong";
-}
-}
+    $username = $_POST['userName'];
+    $password = $_POST['pSd'];
+    $sql = "SELECT * FROM `loginsystem` WHERE `uname`='" . $username . "'AND `psd`='" . $password . "'";
+    $result = $inloggen->query($sql);
+    $num = mysqli_num_rows($result);
 
+    if ($num == 1) {
+        $extra = 'welcome.php';
+        header("Location: $extra");
+    } else {
+        $extra1 = 'registreren.php';
+        header("Location: $extra1");
+    }
+}
 ?>
 
 <html>
